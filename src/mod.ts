67fdc8525d539fd92d304c123d7b5cd8753d16b1
davis9001 <monaghan.david@gmail.com>
2025-02-21 15:47:17 -0500

    5th jsdoc edits/tests

--- src/mod.ts
+++ src/mod.ts
@@ -1,19 +1,1 @@
-/**
- * Fresh ThemeToggle.
- * @module
- *
- * Adds a <ThemeToggle /> Island to a Fresh app.
- *
- * @example
- * ```tsx
- * import { ThemeToggle } from '@davis9001/fresh-theme-toggle/islands';
- * ...
- * return (
- *  <nav>
- *    <ThemeToggle />
- *  </nav>
- * )
- * ```
- * @see {@link https://github.com/davis9001/fresh-theme-toggle}
- */
 export { freshThemeToggle, themeToggleHeadScript } from './plugin.ts';
